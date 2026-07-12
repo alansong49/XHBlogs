1f340d0dc44af5266be247f2c4aee8e0ddeec999
Blog CMS <cms@blog.local>
2026-07-12 21:15:44 +0800

    Auto sync source code for Vercel 🚀

--- data/projects.ts
+++ data/projects.ts
@@ -11,6 +11,14 @@ export type Project = {
 
 export const projectsData: Project[] = [
   {
+    "id": "proj_1783859889933",
+    "name": "yolov一键自动安装",
+    "githubUrl": "https://github.com/alansong49/yolov-one-click-installation/tree/main",
+    "description": "YOLO AutoInstaller 是一款基于 PyQt6 开发的 YOLO 全版本一键部署 GUI 工具，支持 Windows 和 Linux 双平台。旨在帮助用户快速完成 YOLO 环境的安装、配置和部署，无需手动配置复杂的环境变量和依赖。",
+    "icon": "🚀",
+    "tags": []
+  },
+  {
     "id": "proj_1781007486671",
     "name": "Linux 管理器",
     "githubUrl": "https://github.com/alansong49/Linux-system-manager.git",
